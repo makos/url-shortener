525d7fdb9bfa6dd9d2b00ebeb78971dfe2acbaee
makos <643278+makos@users.noreply.github.com>
2022-03-05 15:57:55 +0100

    Add config.js for express

---
+++ src/server/config.js
@@ -0,0 +1,10 @@
+/* ExpressJS server configuration file. Database stuff etc. */
+
+const process = require('process');
+
+module.exports = {
+  db_host: process.env.DB_HOST || "localhost",
+  db_user: process.env.DB_USER || "shorter",
+  db_password: process.env.DB_PASSWORD || "strongpass",
+  db: "SHORTER"
+};
--- src/server/index.js
+++ src/server/index.js
@@ -1,16 +1,17 @@
-import process from 'process';
-
+const process = require('process');
 const express = require('express');
+const mysql = require('mysql');
 const app = express();
 const port = process.env.PORT || 3001;
-const mysql = require('mysql');
+
+const config = require('./config.js');
 
 /* TODO: Config file with all those things inside environment variables. */
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'shorter',
-  password: 'strongpass',
-  database: 'SHORTER'
+  host: config.db_host,
+  user: config.db_user,
+  password: config.db_password,
+  database: config.db
 });
 
 connection.connect();
@@ -24,25 +25,20 @@ app.get('/:urlId', (req, res) => {
     (err, result, fields) => {
       if (err) throw err;
 
-      console.log("Result:", result);
-
       const hasUrl = result.length;
       res
         .status(hasUrl ? 200 : 404)
-        .json(hasUrl ? result[0].URL : {status: "No URL with given ID."});
+        .json(hasUrl ? result[0].longLink : {status: "No URL with given ID."});
     });
 });
 
 app.post('/', (req, res) => {
   console.log("Received request with body: ", req.body);
   connection.query(
-    'INSERT INTO URLS (URL) VALUES (?)',
-    [req.body.longLink],
+    'INSERT INTO URLS SET ?',
+    [req.body],
     (err, result, fields) => {
       if (err) throw err;
-
-      console.log("Result:", result);
-      
       res.status(200).json({shortLink: `myshorter.com/${result.insertId}`});
     }
   );
