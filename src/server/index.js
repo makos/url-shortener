42c859d54bd9de1d13cdd00e2be38f87c05310d3
makos <643278+makos@users.noreply.github.com>
2022-03-05 13:55:55 +0100

    Add basic database querying

--- src/server/index.js
+++ src/server/index.js
@@ -1,22 +1,50 @@
 const express = require('express');
 const app = express();
 const port = 3001;
+const mysql = require('mysql');
 
-const calcId = function (req, res, next) {
-  req.newId = 1;
-  next();
-}
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'shorter',
+  password: 'strongpass',
+  database: 'SHORTER'
+});
+
+connection.connect();
 
 app.use(express.json());
-app.use(calcId);
 
 app.get('/:urlId', (req, res) => {
-  res.send(res.json({url: req.params.urlId}));
+  connection.query(
+    'SELECT * FROM URLS WHERE ID = ?',
+    [req.params.urlId],
+    (err, result, fields) => {
+      if (err) throw err;
+
+      console.log("Result:", result);
+
+      if (result[0]) {
+        res.status(200).json(result[0].URL);
+      } else {
+        res.status(404).json({status: "No URL with given ID."});
+      }
+    });
 });
 
 app.post('/', (req, res) => {
   console.log("Received request with body: ", req.body);
-  res.status(200).json({shortLink: `myshorter.com/${req.newId}`});
+  connection.query(
+    'INSERT INTO URLS (URL) VALUES (?)',
+    [req.body.longLink],
+    (err, result, fields) => {
+      if (err) throw err;
+
+      console.log("Result:", result);
+      console.log("ID: ", result.insertId);
+      
+      res.status(200).json({shortLink: `myshorter.com/${result.insertId}`});
+    }
+  );
 });
 
 app.listen(port, () => {
