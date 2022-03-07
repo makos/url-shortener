3f20779ca326f9b369c6c307e7359b1984f5e89a
makos <643278+makos@users.noreply.github.com>
2022-03-07 21:20:10 +0100

    Add port to config.js

--- src/server/config.js
+++ src/server/config.js
@@ -3,8 +3,9 @@
 const process = require('process');
 
 module.exports = {
-  db_host: process.env.DB_HOST || "localhost",
-  db_user: process.env.DB_USER || "shorter",
-  db_password: process.env.DB_PASSWORD || "strongpass",
-  db: "SHORTER"
+  db_host: process.env.DB_HOST || 'localhost',
+  db_user: process.env.DB_USER || 'shorter',
+  db_password: process.env.DB_PASSWORD || 'strongpass',
+  db: 'SHORTER',
+  port: process.env.PORT || 3001
 };
