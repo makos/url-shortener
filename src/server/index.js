e7479ba5e18c76e274327102838971fd6e7f68e7
makos <643278+makos@users.noreply.github.com>
2022-03-07 21:20:22 +0100

    URL validation with `node-uri`; comments; pull port from config.js

--- src/server/index.js
+++ src/server/index.js
@@ -3,7 +3,6 @@ const express = require('express');
 const mysql = require('mysql');
 const app = express();
 const uri = require('node-uri');
-const port = process.env.PORT || 3001;
 
 const config = require('./config.js');
 
@@ -19,16 +18,20 @@ connection.connect();
 app.use(express.json());
 
 app.get('/:urlId', (req, res) => {
+  /* MariaDB SQL queries via mysql module. */
+  /* TODO: Move this out into separate file. */
   connection.query(
     'SELECT * FROM URLS WHERE ID = ?',
     [req.params.urlId],
     (err, result, fields) => {
-      if (err) throw err;
-
+      if (err) {
+        res.status(500).json({shortLink: 'Server error. Please try again.'});
+        return;
+      }
+      /* The query returns an array even if there's no records. Check if the
+         array has any members (should only ever have one because we use unique primary
+         keys) and then act accordingly. */
       const hasUrl = result.length;
-      /* TODO: URL checking; don't try to redirect to invalid HTTP address!
-         Maybe only need to be checked in the POST route? If it passes there we
-         can assume it's valid here. */
       if (hasUrl) {
         res.redirect(301, result[0].longLink);
       } else {
@@ -38,22 +41,34 @@ app.get('/:urlId', (req, res) => {
 });
 
 app.post('/', (req, res) => {
-  /* TODO: Remove console logging after dev. */
-  console.log('Received request with body:' , req.body);
-  /* TODO: URL validation; has the user sent in a valid URL? */
+  /* Validate the URL with node-uri. */
+  try {
+    uri.checkWebURL(req.body);
+  } catch (uriError) {
+    res.status(400).json({shortLink: 'Bad URL, please check your input.'});
+    return;
+  }
+  /* MariaDB SQL queries via mysql module. */
+  /* TODO: Move this out into separate file. */  
   connection.query(
     'INSERT INTO URLS SET ?',
     [req.body],
     (err, result, fields) => {
-      /* TODO: Error handling. */
-      if (err) throw err;
-      /* TODO: Dev / debug stuff. */
+      /* Error handling. */
+      if (err) {
+        res.status(500).json({shortLink: 'Server error. Please try again.'});
+        return;
+      }
+
+      if (app.get('dev') === 'development') {
+        const port = ':3001';
+      }
       
-      res.status(200).json({shortLink: `${req.hostname}/${result.insertId}`});
+      res.status(200).json({shortLink: `${req.hostname}${port}/${result.insertId}`});
     }
   );
 });
 
-app.listen(port, () => {
-  console.log(`Backend listening on port ${port}.`);
+app.listen(config.port, () => {
+  console.log(`Backend listening on port ${config.port}.`);
 });
