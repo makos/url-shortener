40877ee04bd870b1bf895f2473faa69d9cbfa9e9
makos <643278+makos@users.noreply.github.com>
2022-03-07 20:15:14 +0100

    Add clipboard copying to displayed Link

--- src/Link.js
+++ src/Link.js
@@ -1,10 +1,37 @@
+import './Link.css'
 import React from 'react';
 
 class Link extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.copyLink = this.copyLink.bind(this);
+    this.state = {
+      copied: false
+    };
+  }
+
+  copyLink(e) {
+    e.preventDefault();
+    
+    navigator.clipboard.writeText(this.props.shortLink)
+      .then(() => this.setState({copied: true}),
+            () => console.error("Error copying to clipboard. Check HTTPS."));
+  }
+  
   render() {
+    const icon = this.state.copied ? <span>&#10004;</span> : <span>&#9112;</span>
+    
     return (
       <div>
-        <p>{this.props.shortLink ? this.props.shortLink : null}</p>
+        {this.props.shortLink
+         ? (<p>
+              {this.props.shortLink} <a href="#" onClick={this.copyLink}>
+                                       {icon}
+                                     </a>
+            </p>)
+         : null
+        }
       </div>
     )
   }
